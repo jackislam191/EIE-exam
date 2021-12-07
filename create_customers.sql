487d7b563cca7d450e81be0533c04e64924b3373
jack lam <jackislam191@gmail.com>
2021-12-08 01:48:43 +0800

    remove old sqldump

--- create_customers.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE `eie4432`.`customers` ( `id` INT NOT NULL AUTO_INCREMENT , `nickname` VARCHAR(255) NOT NULL , `password` VARCHAR(255) NOT NULL , `email` VARCHAR(255) NOT NULL , `profile_image` VARCHAR(255) NOT NULL , `gender` VARCHAR(255) NOT NULL , `birthday` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
